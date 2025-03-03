e1e5bacb119c8cd67dc1cab1b61d2d130718f785
TangangMcBride <tangang.mcbride@ictuniversity.edu.cm>
2025-03-03 02:03:34 +0100

    add user routes and controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,8 @@
+//@desc Auth user /set token
+//@route POST /api/users/auth
+//@access Public
+
+const authUser = async (req, res) => {
+  res.status(200).json({ message: "User Authenticated" });
+};
+ export { authUser };
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => 
-  console.log(`Server is running on http://localhost:${port}`));
+app.use("/api/users", userRoutes);
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () =>
+  console.log(`Server is running on http://localhost:${port}`)
+);
