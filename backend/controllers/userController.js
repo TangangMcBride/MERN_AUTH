fa964b08b6ffb9d4e960e6eb743775a9532f7b15
TangangMcBride <tangang.mcbride@ictuniversity.edu.cm>
2025-03-04 18:35:10 +0100

    Added all routes and controllers for user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,43 @@
+import asyncHandler from "express-async-handler";
+
 //@desc Auth user /set token
 //@route POST /api/users/auth
 //@access Public
-
-const authUser = async (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "User Authenticated" });
+});
+
+//@desc register user
+//@route POST /api/users/auth
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User Registered" });
+});
+//@desc Logout user
+//@route POST /api/users/logout
+//@access Public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User LoggedOut" });
+});
+
+//@desc Get user profile
+//@route GET /api/users/profile
+//@access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: " Get User Profile" });
+});
+
+//@desc Update User Profile
+//@route PUT /api/users/profile
+
+//@access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update User Profile" });
+});
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile
 };
- export { authUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,19 @@
- import express from "express";
-import { authUser } from "../controllers/userController.js";
+import express from "express";
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController.js";
 const router = express.Router();
 
-router.post("/auth", authUser);
+router.post("/", registerUser);
+router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router.get('/profile', getUserProfile);
+router.put('/profile', updateUserProfile);
 
 
 
-export default router
+export default router;
