fccb52c9022bca43807ae379cea58c596216d1a2
TangangMcBride <tangang.mcbride@ictuniversity.edu.cm>
2025-03-06 16:30:27 +0100

    Added authMiddleware.js

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 connectDB();
 const port = process.env.PORT || 5000;
 const app = express();
+app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use("/api/users", userRoutes);
