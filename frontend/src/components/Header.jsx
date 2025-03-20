e644d8d4fc08b987fe7f1395360fa474d4ee1778
TangangMcBride <tangang.mcbride@ictuniversity.edu.cm>
2025-03-20 08:21:41 +0100

    update Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,29 +1,43 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown, Badge } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 
- import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const {userInfo} = useSelector((state) => state.auth);
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          
-            <Navbar.Brand as={Link} to='/'>MERN APP</Navbar.Brand>
-          
+          <Navbar.Brand as={Link} to="/">
+            MERN APP
+          </Navbar.Brand>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              
-                <Nav.Link as={Link} to='/login'>
-                  <FaSignInAlt /> sign In
-                </Nav.Link>
-           
-              
-                <Nav.Link as={Link} to='/register'>
-                  <FaSignOutAlt /> Sign Up
-                </Nav.Link>
-              
+              {userInfo ? (
+                <>
+                  <NavDropdown title={userInfo.name} id="username">
+                    <Nav.Link as={Link} to="/profile">
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </Nav.Link>
+                    <NavDropdown.Item>Logout</NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              ) : (
+                <>
+                  <Nav.Link as={Link} to="/login">
+                    <FaSignInAlt /> sign In
+                  </Nav.Link>
+
+                  <Nav.Link as={Link} to="/register">
+                    <FaSignOutAlt /> Sign Up
+                  </Nav.Link>
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
