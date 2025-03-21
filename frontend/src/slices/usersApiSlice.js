f30d10d379d2854bec0d65cdfa01df54d4174bca
TangangMcBride <tangang.mcbride@ictuniversity.edu.cm>
2025-03-21 17:39:21 +0100

    update usersApiSlice

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,16 +1,36 @@
 import { apiSlice } from "./apiSlice";
-const USERS_URL = "/api/users/";
+const USERS_URL = "/api/users";
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
-        url: `${USERS_URL}auth/`,
+        url: `${USERS_URL}/auth`,
         method: "POST",
+        body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: "POST",
+      }),
+    }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    updateUser: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: "PUT",
         body: data,
       }),
     }),
   }),
 });
 
-export const { useLoginMutation } = usersApiSlice;  
+export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useUpdateUserMutation} = usersApiSlice;
