dc25e8e3595e31aa80f92f9dfa49b3cfe4f540fd
TangangMcBride <tangang.mcbride@ictuniversity.edu.cm>
2025-03-21 17:36:07 +0100

    Add PrivateRoute

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,10 +12,11 @@ import store from "./store.js";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import PrivateRoute from "./components/PrivateRoute.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
+import ProfileScreen from "./screens/ProfileScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,14 +24,17 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+      <Route path="" element={<PrivateRoute/>}>
+        <Route path="/profile" element={<ProfileScreen />} />
+      </Route>
     </Route>
   )
 );
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
   </Provider>
 );
