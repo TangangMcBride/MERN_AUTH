4c0283740f6f0c3a90bbf48de1c6c1b5dcf50768
TangangMcBride <tangang.mcbride@ictuniversity.edu.cm>
2025-03-21 17:37:41 +0100

    Add Loader component

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,21 @@
+import { Spinner } from "react-bootstrap"
+
+const Loader = () => {
+  return (
+    <Spinner 
+    animation="border" 
+    variant="primary"
+    role="status"
+    style={{
+        margin:'auto',
+        display:'block'
+    }
+
+    }
+    
+    
+     />
+  )
+}
+
+export default Loader
