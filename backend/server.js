1ba7d6f9421275c3986a1b828464215fee163692
TangangMcBride <tangang.mcbride@ictuniversity.edu.cm>
2025-03-23 15:22:58 +0100

    Add deployment settings

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,23 @@ import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
+
 dotenv.config();
 connectDB();
 const port = process.env.PORT || 5000;
 const app = express();
+app.use(cors())
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
+
+if(process.env.NODE_ENV !== 'production'){
+  
+}
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.use(notFound);
